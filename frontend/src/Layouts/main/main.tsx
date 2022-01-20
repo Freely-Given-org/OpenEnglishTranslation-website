e0e33c62d0fd9fdfb403b1e7ec8bf5b4fb0964eb
Robert <Freely.Given.org+BOS@gmail.com>
2022-01-20 16:17:55 +1300

    Try an image

--- frontend/src/Layouts/main/main.tsx
+++ frontend/src/Layouts/main/main.tsx
@@ -1,12 +1,7 @@
-import logo from '../../Assets/logo512.png';
 import './main.scss';
 
 function Main() {
-    return (
-        <div className="main">
-            
-        </div>
-    );
+    return <div className="main"></div>;
 }
 
 export default Main;
--- frontend/src/Pages/Home/Home.tsx
+++ frontend/src/Pages/Home/Home.tsx
@@ -7,6 +7,23 @@ function Home() {
         <>
             <Header />
             <Main />
+            <h1>Exciting new Bible translation!</h1>
+            <p>
+                Yes, the design of the <i>Open English Translation</i> of the
+                Bible is now proceeding and actual translation work is set to
+                resume by the middle of this year (2022).
+            </p>
+            <p>
+                Currently, the best place to read about it is at{' '}
+                <a href="https://freely-given.org/BibleTranslations/English/OET/">
+                    Freely-Given.org
+                </a>
+                , but this will soon become the main site.
+            </p>
+            <img
+                alt="Photo of Bible"
+                src="../../../Assets/sixteen-miles-out-MLOZazGGCb8-unsplash.jpg"
+            />
             <Footer />
         </>
     );
