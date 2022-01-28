083dbbe364296453c1d46be64c25667185d3fbbb
Night_Hunter <samuelhuntnz@gmail.com>
2022-01-28 17:50:33 +1300

    added new page components

---
+++ frontend/src/Components/page/MainPageContent/MainPageContent.tsx
@@ -0,0 +1,7 @@
+import './main-page-content.scss';
+
+function MainPageContent({ children }: any) {
+    return <div className='main-page-content'>{children}</div>;
+}
+
+export default MainPageContent;
