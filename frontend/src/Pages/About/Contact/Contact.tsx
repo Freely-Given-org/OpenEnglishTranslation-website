40ad69163b9a77ab8ae2de345c61aa483acc2211
Robert <Freely.Given.org+BOS@gmail.com>
2022-02-11 17:10:33 +1300

    Add some text

--- frontend/src/Pages/About/Contact/Contact.tsx
+++ frontend/src/Pages/About/Contact/Contact.tsx
@@ -1,7 +1,17 @@
 import Main from '../../../Layouts/main/main';
 
 function Contact() {
-    return <Main></Main>;
+    return (
+        <Main>
+            <h1>Contacting the Editors</h1>
+            <p>
+                The Open English Translation of the Bible is a product of{' '}
+                <a href='https://Freely-Given.org'>Freely-Given.org</a>. To
+                contact the editors with corrections or suggestions, please
+                email Freely (dot) Given (dot) org (at) gmail (dot) com.
+            </p>
+        </Main>
+    );
 }
 
 export default Contact;
