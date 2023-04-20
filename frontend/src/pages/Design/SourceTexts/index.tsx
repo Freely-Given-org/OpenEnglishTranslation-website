58faadacc890a0f6b4722f7fa62c54024bf017fa
Robert <Freely.Given.org+BOS@gmail.com>
2023-04-20 15:41:19 +1200

    Filled out final pages

--- frontend/src/pages/Design/SourceTexts/index.tsx
+++ frontend/src/pages/Design/SourceTexts/index.tsx
@@ -14,7 +14,8 @@ function SourceTexts() {
                 <Page>
                     <MainPageContent>
                         <div>
-                            <h1>Hebrew Scriptures</h1>
+                            <h1>Hebrew and Greek Source Texts</h1>
+                            <h2>Hebrew Scriptures</h2>
                             <p>
                                 We are very grateful for the work of the Open
                                 Scriptures Hebrew Bible team, and will be using
@@ -26,7 +27,7 @@ function SourceTexts() {
                                 This text is based on the Westminster Lenigrad
                                 Codex (WLC).
                             </p>
-                            <h1>Greek Translation of Hebrew Scriptures</h1>
+                            <h2>Greek Translation of Hebrew Scriptures</h2>
                             <p>
                                 We are still in the process of finding a
                                 suitable open-licensed copy of the{' '}
@@ -35,9 +36,11 @@ function SourceTexts() {
                                 </Link>{' '}
                                 (LXX) Greek translation of the Hebrew
                                 Scriptures. This translation was done around
-                                250BC.
+                                250BC and is important because it appears to
+                                have had a major influence on the Greek
+                                Scriptures.
                             </p>
-                            <h1>Greek Scriptures</h1>
+                            <h2>Greek Scriptures</h2>
                             <p>
                                 We are thrilled to be among the very early users
                                 of the ground-breaking{' '}
@@ -45,6 +48,39 @@ function SourceTexts() {
                                 Testament from the{' '}
                                 <Link href='https://GreekCNTR.org'>
                                     Center for New Testament Restoration
+                                </Link>{' '}
+                                and would like to express our thanks to Dr. Alan
+                                Bunning for the many years of (volunteer and
+                                later, sponsored) work that he put into this
+                                modern, high-quality, open-licensed Greek New
+                                Testament.
+                            </p>
+                            <h1>Other translations and source texts</h1>
+                            <p>
+                                The other open-licensed translations and
+                                original language texts that can be found in the
+                                parallel and interlinear pages in our{' '}
+                                <Link href='/Reader'>Bible Reader</Link> each
+                                have their own copyright details pages, but we
+                                especially note here the contribution of{' '}
+                                <Link href='https://eBible.org'>
+                                    eBible.org
+                                </Link>{' '}
+                                which is a huge help, including the
+                                open-licensed{' '}
+                                <Link href='https://WorldEnglish.Bible/'>
+                                    World English Bible
+                                </Link>{' '}
+                                which is part of the inspiration of this work.
+                            </p>
+                            <h2>Translation Notes</h2>
+                            <p>
+                                The Translation Notes that can be found in the
+                                parallel and interlinear pages in our{' '}
+                                <Link href='/Reader'>Bible Reader</Link> come
+                                from{' '}
+                                <Link href='https://unfoldingWord.org'>
+                                    unfoldingWord.org
                                 </Link>
                                 .
                             </p>
