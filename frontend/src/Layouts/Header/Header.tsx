056ccbcb425475109cf29e08394edf970b0c4db5
Night_Hunter <samuelhuntnz@gmail.com>
2024-01-28 15:49:34 +1300

    Update link components in Footer and Header modules

--- frontend/src/Layouts/Header/Header.tsx
+++ frontend/src/Layouts/Header/Header.tsx
@@ -1,8 +1,8 @@
 import CloseIcon from '@mui/icons-material/Close';
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import { NextPage } from 'next';
-import Link from 'next/link';
 import { useState } from 'react';
+import NextLink from 'next/link';
 
 import { BigHomeLogo, SmallHomeLogo } from '../../Assets/SVGs';
 import SideMenu from '../../Components/menu/SideMenu/SideMenu';
@@ -10,6 +10,7 @@ import About from '../../Components/navDropdowns/About';
 import Design from '../../Components/navDropdowns/Design';
 import Resources from '../../Components/navDropdowns/Resources';
 import styles from './Header.module.scss';
+import { Link } from '@mui/material';
 
 const Header: NextPage = () => {
     const [sideMenu, setsideMenu] = useState(false);
@@ -17,18 +18,16 @@ const Header: NextPage = () => {
     return (
         <div className={styles['header']}>
             <div className={styles['main-logo']}>
-                <Link href={'/'}>
-                    <a>
-                        <BigHomeLogo className={styles['big-main-logo']} />
-                        <SmallHomeLogo className={styles['small-main-logo']} />
-                    </a>
+                <Link component={NextLink} href={'/'}>
+                    <BigHomeLogo className={styles['big-main-logo']} />
+                    <SmallHomeLogo className={styles['small-main-logo']} />
                 </Link>
             </div>
             <nav className={styles['big-nav']}>
                 <Design />
-                <Link href='/Reader'>Reader</Link>
+                <Link component={NextLink}  href='/Reader'>Reader</Link>
                 <Resources />
-                <Link href={'/Blog'}>Blog</Link>
+                <Link component={NextLink}  href={'/Blog'}>Blog</Link>
                 <About />
             </nav>
 
