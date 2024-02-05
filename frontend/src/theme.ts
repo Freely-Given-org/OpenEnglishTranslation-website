28363636af67f815e9b18047e6695561c0fc133f
Night_Hunter <samuelhuntnz@gmail.com>
2024-02-05 14:16:00 +1300

    Import createTheme from lib/mui and fix typo in DJANGO_SERVER_URL

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,6 +1,6 @@
 'use client';
+import { createTheme } from 'lib/mui';
 import { Roboto } from 'next/font/google';
-import { createTheme } from '@mui/material/styles';
 
 const roboto = Roboto({
   weight: ['300', '400', '500', '700'],
