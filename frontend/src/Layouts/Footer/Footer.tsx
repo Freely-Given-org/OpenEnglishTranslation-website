3f7931335b29c37e5ce06c5a436b13630d68bd11
Robert <Freely.Given.org+BOS@gmail.com>
2024-10-29 10:39:41 +1300

    Late October update

--- frontend/src/Layouts/Footer/Footer.tsx
+++ frontend/src/Layouts/Footer/Footer.tsx
@@ -104,7 +104,7 @@ function Footer() {
                     </a>
                 </span>
                 <span className={styles['buildNotes']}>
-                    (Site last built: 26 October 2024)
+                    (Site last built: 29 October 2024)
                 </span>
             </div>
         </div>
