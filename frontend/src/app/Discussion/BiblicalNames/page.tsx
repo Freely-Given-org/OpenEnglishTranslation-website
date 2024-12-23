d495e3fcca24c957b2fbf2a88c010bf3edcb9379
Robert <Freely.Given.org+BOS@gmail.com>
2024-12-24 08:15:21 +1300

    More on names

--- frontend/src/app/Discussion/BiblicalNames/page.tsx
+++ frontend/src/app/Discussion/BiblicalNames/page.tsx
@@ -35,6 +35,48 @@ function BiblicalNames() {
                             ignorance.
                         </p>
                         <p>
+                            The main problem is that names of characters from
+                            the Hebrew Scriptures (commonly called the ‘Old
+                            Testament’){' '}
+                            <b>
+                                went from Hebrew to Greek to Latin to
+                                middle-English (influenced by German spelling)
+                                to modern-English
+                            </b>
+                            , so by time we get them in our language, they don’t
+                            look/sound very Hebrew any more.
+                        </p>
+                        <p>
+                            ‘What does it matter?’ you might ask. Well,
+                            hopefully you would realise that speakers of another
+                            language might mispronounce your name. ‘Robert’ is
+                            often pronounced more like ‘Luber’ by Korean
+                            speakers. (Some languages don’t have ‘r’ or don’t
+                            distinguish ‘l’ from ‘r’. Some languages don’t allow
+                            two consonants to be together in one syllable
+                            (called ‘consonant clusters’). So ‘Luber’ would be a
+                            good approximation of ‘Robert’—at least it’s got the
+                            right number of syllables. What we would hope is
+                            that it’s pronounced with respect, i.e., that the
+                            Korean speaker made an effort to pronounce it as
+                            closely as possible to how I would say it. Compare
+                            that to the three syllable name ‘Yeshu'a’. Most
+                            English dialects don’t use the glottal stop so we
+                            can easily understand it coming out as ‘Yeshuwa’.
+                            Now English has all those sounds. But ‘Jesus’ seems
+                            miles off. The first consonant is wrong, the ‘sh’ is
+                            unnecessarily changed to ‘s’ (because Greek doesn’t
+                            have ‘sh’ even though English does), and the final
+                            ‘s’ consonant is unnecessarily added (when English,
+                            unlike some other languages, DOES allow syllables to
+                            end with a vowel) and it’s been reduced to two
+                            syllables from three. (The reason that we’ve ended
+                            up with ‘Jesus’ is that the Greek ‘Yaysous’ went
+                            through some English spelling and pronunciation
+                            changes and ending up starting with the modern ‘j’
+                            sound.)
+                        </p>
+                        <p>
                             The <em>Open English Translation</em> of the Bible
                             has a goal of improving Biblical understanding, and
                             this extends to trying to improve the accuracy of
@@ -53,6 +95,13 @@ function BiblicalNames() {
                             Consonants in the relevant languages
                         </h2>
                         <p>
+                            The following section is a layman’s look at the
+                            consonants in the languages that have had the most
+                            influence on transliterating Biblical names from
+                            Hebrew and Koine Greek into our modern-English
+                            Bibles.
+                        </p>
+                        <p>
                             <b>Hebrew</b> has twenty-two consonants in its
                             alphabet which can be approximated into English as:
                             <br />
@@ -60,7 +109,7 @@ function BiblicalNames() {
                             <b>glottal stop</b>.
                             <br />
                             <small>
-                                (The astute reader will notice that that's only
+                                (The astute reader will notice that that’s only
                                 eighteen consonants, and that’s because Hebrew
                                 has doubles of some consonants, which are
                                 pronounced slightly differently, but which we
@@ -76,7 +125,8 @@ function BiblicalNames() {
                             Testament’) has seventeen consonants in its alphabet
                             which can be approximated into English as:
                             <br />
-                            <b>b ch d g k l m n p ph ps r s t th x and z</b>.
+                            <b>b ch d g k l m n p ph ps r s t th x</b> and{' '}
+                            <b>z</b>.
                             <br />
                             Note particularly that{' '}
                             <b>
@@ -95,7 +145,8 @@ function BiblicalNames() {
                             in its alphabet which can be approximated into
                             English as:
                             <br />
-                            <b>b c d f g h k l m n p r s t v x y and z</b>.
+                            <b>b c d f g h k l m n p r s t v x y</b> and{' '}
+                            <b>z</b>.
                             <br />
                             Note particularly that{' '}
                             <b>
@@ -112,7 +163,9 @@ function BiblicalNames() {
                             from) has twenty plus consonants in its alphabet
                             which can be approximated into English as:
                             <br />
-                            <b>b c d f g h k l m n p q r s ß t v x y and z</b>.
+                            <b>
+                                b c d f g h k l m n p q r s ß t v x y
+                            </b> and <b>z</b>.
                             <br />
                             Note particularly that their letter ‘j’ is
                             approximated by English letter ‘y’, as in their word
@@ -126,7 +179,8 @@ function BiblicalNames() {
                             <b>Modern English</b> has the following consonant
                             sounds in its alphabet:
                             <br />
-                            <b>b d f g h j k l m n p r s t v w x y and z</b>.
+                            <b>b d f g h j k l m n p r s t v w x y</b> and{' '}
+                            <b>z</b>.
                             <br />
                             Note that ‘c’ and ‘q’ are omitted from the above
                             list because they don’t have unique sounds of their
@@ -148,11 +202,14 @@ function BiblicalNames() {
                             the consonants.)
                         </p>
                         <h2 id='lxx'>The first major Bible translation</h2>
+                        <p>Coming...</p>
                         <h2 id='history'>
                             The historical accidents that led to our Biblical
                             names
                         </h2>
+                        <p>Coming...</p>
                         <h2 id='trends'>Modern cultural trends</h2>
+                        <p>Coming...</p>
                         <h2>More coming...</h2>
                     </MainPageContent>
                 </Page>
