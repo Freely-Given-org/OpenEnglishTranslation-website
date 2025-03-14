cc9b0dbcd085a6acf298d6d1f1123ab1ccc74aab
Robert <Freely.Given.org+BOS@gmail.com>
2025-03-15 12:39:32 +1300

    Small fix plus a couple of small updates

--- frontend/src/Layouts/Footer/Footer.tsx
+++ frontend/src/Layouts/Footer/Footer.tsx
@@ -104,7 +104,7 @@ function Footer() {
                     </a>
                 </span>
                 <span className={styles['buildNotes']}>
-                    (Site last built: 14 March 2025)
+                    (Site last built: 15 March 2025)
                 </span>
             </div>
         </div>
--- frontend/src/app/Discussion/Notes/page.tsx
+++ frontend/src/app/Discussion/Notes/page.tsx
@@ -18,7 +18,7 @@ function Notes() {
                         <h1>Notes</h1>
                         <h2>Project stages</h2>
                         <p>
-                            We are currently in first-draft mode. This means
+                            We are currently in ‘first-draft mode’. This means
                             that we're trying to fill in all 31,102 Bible verses
                             with text, so that we have a Bible that we can at
                             least start reading and find every reference.
@@ -30,21 +30,25 @@ function Notes() {
                             <li>
                                 Have the computer output a very literal English
                                 version of each word in the original texts
-                                (using both our own and others’ resources)
+                                (using both our own and others’ resources that
+                                are open-licensed)
                             </li>
                             <li>
                                 Re-order those sentences to make them readable
                                 in English, thus producing our{' '}
-                                <em>Literal Version</em>
+                                <em>OET Literal Version</em>
                             </li>
                             <li>
-                                Manually (i.e., not using so-called AI tools)
+                                Manually (i.e., not using so-called ‘AI’ tools)
                                 produce a modern-English draft of each sentence
                                 from that <em>Literal Version</em> (using our
-                                parallel verse pages which include other
-                                resources, and our word pages and other online
-                                lexicons, etc.), thus producing a first draft of
-                                our <em>Readers’ Version</em>
+                                <Link href='https://Freely-Given.org/OBD/par/MRK/C1V1.htm#Top'>
+                                    parallel verse pages
+                                </Link>{' '}
+                                which include other resources, and our word
+                                pages and other online lexicons, etc.), thus
+                                producing a first draft of our{' '}
+                                <em>OET Readers’ Version</em>
                             </li>
                             <li>
                                 Rework the <em>Readers’ Version</em> to be more
@@ -60,11 +64,12 @@ function Notes() {
                                 cross-references
                             </li>
                         </ol>
+                        <p>So we’re currently at stage #3 above.</p>
                         <h1>Disclaimers</h1>
                         <h2>Translation distance marking</h2>
                         <p>
-                            In our <em>Readers’ Version</em> translation, we now
-                            attempt to mark up where we change from a literal
+                            In our <em>OET Readers’ Version</em> translation, we now
+                            attempt to mark up where we change it from a literal
                             translation, e.g., if we change the ‘The wealthy man
                             says’ to ‘Wealthy people say’ then we've made a
                             change in <em>number</em> (i.e., from singular to
@@ -84,7 +89,7 @@ function Notes() {
                         </p>
                         <p>
                             <small>
-                                Note: This additional markup will only be
+                                Note: That additional markup will only be
                                 visible in online versions—only ‘added’ words
                                 will be visible in print versions (deemphasised
                                 in gray—not emphasied in <i>italics</i> like in
