24ab0d04e846588c4a4d2eef6c49c512f79fb1b3
Samuel Hunt <samuelhuntnz@gmail.com>
2026-04-10 20:15:21 +1200

    remove old frontend and backend code

--- frontend/src/Components/menu/SideMenu/SideMenu.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './side-menu.module.scss';
-
-function SideMenu({ children }: any) {
-    return <div className={styles['side-menu']}>{children}</div>;
-}
-
-export default SideMenu;
--- frontend/src/Components/page/MainPageContent/SideBar/SideBar.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './side-bar.module.scss';
-
-function SideBar({ children }: any) {
-    return <div className={styles['side-bar']}>{children}</div>;
-}
-
-export default SideBar;
